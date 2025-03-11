8c88ff15909f7c2718535979dafe9878f840a3c9
Karl255 <49595438+Karl255@users.noreply.github.com>
2025-03-11 20:54:47 +0100

    setup react-router

---
+++ src/pages/Bar.jsx
@@ -0,0 +1,16 @@
+export function Bar() {
+    return (
+        <>
+            <h1>Bar page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad esse error nulla quis corrupti? Repellat,
+                reiciendis modi vero voluptatibus aspernatur qui nam. Expedita repellendus minima, consectetur inventore
+                repellat praesentium sapiente id odio! Fugiat quae et quis saepe repellat quas iste maiores assumenda,
+                aut esse itaque. Temporibus similique molestiae doloremque vel veniam, eos illo sequi a, nam excepturi
+                tempora amet nihil? Harum exercitationem, repellat quasi veniam delectus quia vel natus a, libero neque
+                totam vero nulla eaque inventore deleniti ipsam quae? Natus, ea? Iure, quae aperiam labore hic odit
+                officiis dolorem repellendus, reprehenderit ex numquam sed aliquid veritatis sunt, explicabo non.
+            </p>
+        </>
+    );
+}
