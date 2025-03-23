310b5af62b6881861bd20008d61264e9d4bd88b4
mkis2001 <134554026+mkis2001@users.noreply.github.com>
2025-03-23 21:46:37 +0100

    Frontend1 (#2)

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from "react";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { Foo } from "./pages/Foo";
-import { Bar } from "./pages/Bar";
+import React, { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router';
+import { Navigation } from './pages/Navigation/Navigation';
+import { ObjaveContextProvider } from './contexts/ObjaveContextProvider';
+
+import { Container } from '@mui/material';
 
 function App() {
     return (
         <StrictMode>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Foo />} />
-                    <Route path="/bar" element={<Bar />} />
-                </Routes>
-            </BrowserRouter>
+            <ObjaveContextProvider>
+                <BrowserRouter>
+                    <Container maxWidth="md">
+                        <Navigation />
+                    </Container>
+                </BrowserRouter>
+            </ObjaveContextProvider>
         </StrictMode>
     );
 }
---
+++ Web/src/pages/Inbox/Inbox.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Inbox() {
+    return <h1>Inbox</h1>;
+}
