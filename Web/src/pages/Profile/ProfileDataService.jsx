aa369af705cf9bbedcece48a1199073cedbecb55
mkis2001 <2001matej@gmail.com>
2025-04-19 12:53:51 +0200

    Changed ProfileDataService to use callApi function

--- Web/src/pages/Profile/ProfileDataService.jsx
+++ Web/src/pages/Profile/ProfileDataService.jsx
@@ -1,33 +1,11 @@
 import { callApi } from '../../DataService';
 
 export async function getProfile({ route, id }) {
-    const response = await fetch(`http://localhost:8080/api/${route}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ idKorisnik: id }),
-    });
-    const res = await response.json();
-    if (!response.ok) {
-        throw new Error(res.message);
-    }
-    return res;
+    return callApi({ method: 'POST', route, data: { idKorisnik: id } });
 }
 
 export async function follow({ idUser, idFollow }) {
-    const response = await fetch(`http://localhost:8080/api/follow`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ idKorisnik: idUser, idZapratiti: idFollow }),
-    });
-    const res = await response.json();
-    if (!response.ok) {
-        throw new Error(res.message);
-    }
-    return res;
+    return callApi({ method: 'POST', route: 'follow', data: { idKorisnik: idUser, idZapratiti: idFollow } });
 }
 
 export async function unfollow({ idUser, idFollow }) {
